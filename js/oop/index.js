544006c9fbda5cd9be75c2cb47b2e9289fcad291
AHKH1990 <135972116+AHKH1990@users.noreply.github.com>
2023-08-07 18:41:30 +0300

    oopupdate

--- js/oop/index.js
+++ js/oop/index.js
@@ -8,5 +8,14 @@ class Location {
     addActivity(activity) {
       this.activities.push(activity);
     }
+  
+    removeActivity(activity) {
+      const index = this.activities.indexOf(activity);
+      if (index !== -1) {
+        this.activities.splice(index, 1);
+      }
+    }
+  
+    }
 
-   }
+  
