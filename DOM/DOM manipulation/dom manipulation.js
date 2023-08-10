45f5c1544deb39df02dd6eae4565e86fce87fa17
AHKH1990 <135972116+AHKH1990@users.noreply.github.com>
2023-08-10 17:17:39 +0300

    newupdate

---
+++ DOM/DOM manipulation/dom manipulation.js
@@ -0,0 +1,4 @@
+const myLi = document.querySelector('li.start-here');
+
+const myLi = document.querySelector('li.start-here');
+myLi.textContent = 'main title';
--- js/oop/index.js
+++ js/oop/index.js
@@ -8,14 +8,57 @@ class Location {
     addActivity(activity) {
       this.activities.push(activity);
     }
-  
+
     removeActivity(activity) {
-      const index = this.activities.indexOf(activity);
-      if (index !== -1) {
-        this.activities.splice(index, 1);
+        const index = this.activities.indexOf(activity);
+        if (index > -1) {
+          this.activities.splice(index, 1);
+        }
+      }
+    
+      displayLocation() {
+        let activityList = this.activities.join(", ");
+        return `Location: ${this.name}\nDescription: ${this.description}\nActivities: ${activityList}`;
       }
     }
   
-    }
-
+    
+    // USE CASE
+  
+  
+    // Create a new location object
+  const beachLocation = new Location(
+      "Sandy Beach",
+      "A beautiful beach with crystal clear water.",
+      ["Swimming", "Sunbathing", "Beach Volleyball"]
+    );
+    
+    // Add activities
+    beachLocation.addActivity("Snorkeling");
+    beachLocation.addActivity("Beach Party");
+    
+    // Display the location information
+    console.log(beachLocation.displayLocation());
+    
+    // Remove an activity
+    beachLocation.removeActivity("Beach Volleyball");
+    
+    // Display the updated location information
+    console.log(beachLocation.displayLocation());
+    
+  
+  
+  
+    
+    // output
+  
+  
+  
+    Location: Sandy Beach
+  Description: A beautiful beach with crystal clear water.
+  Activities: Swimming, Sunbathing, Beach Volleyball, Snorkeling, Beach Party
+  
+  Location: Sandy Beach
+  Description: A beautiful beach with crystal clear water.
+  Activities: Swimming, Sunbathing, Snorkeling, Beach Party
   
