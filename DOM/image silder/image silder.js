99580ea35b25d1aded6280830a6376bf0e91dabb
AHKH1990 <135972116+AHKH1990@users.noreply.github.com>
2023-08-20 13:59:59 +0300

    updatedDomExercises

--- DOM/image silder/image silder.js
+++ DOM/image silder/image silder.js
@@ -1,24 +1,17 @@
-// Select all <img> elements within the "slider" div
 const slides = document.querySelectorAll('#slider img');
 
-// Initialize currentIndex to keep track of the currently displayed image
 let currentIndex = 0;
 
-// Function to move between slides
 function moveSlide(direction) {
-  // Remove the "active" class from the currently displayed image
   slides[currentIndex].classList.remove('active');
 
-  // Adjust the currentIndex
   currentIndex += direction;
 
-  // Handle boundary conditions
   if (currentIndex < 0) {
     currentIndex = 0;
   } else if (currentIndex >= slides.length) {
     currentIndex = slides.length - 1;
   }
 
-  // Add the "active" class to the new current image
   slides[currentIndex].classList.add('active');
 }
